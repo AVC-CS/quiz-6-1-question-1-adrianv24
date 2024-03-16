807ff0440823d941f9c5c64922888c9efa48bbdb
adrianv24 <avskeleton@gmail.com>
2024-03-15 23:33:30 -0700

    r

--- main.hpp
+++ main.hpp
@@ -1,4 +1,7 @@
 
+#include <iostream>
+using namespace std;
+#include <cmath>
 //********************
 // Declare Function Prototypes
 //********************
@@ -6,3 +9,43 @@
 // ******************************
 // Implement all your functions here
 // ******************************
+
+void getTwoValues (int& begin, int& end) {
+    do { cin >> begin >> end;
+    if (begin > end) {
+        cout << "Begin must be less than End" << endl;
+
+
+    }
+    }
+    while (begin > end);
+}
+    int getPrevPrime(int end) {
+        int prev = end - 1;
+        while (true) {
+
+            bool isPrime = true;
+            for (int i=2; i <=sqrt(prev); ++i) {
+                if (prev % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) return prev;
+            prev--;
+        }
+    }
+    int getNextPrime(int begin) {
+        int next = begin +1;
+        while(true) {
+            bool isPrime=true;
+            for(int i=2; i<=sqrt(next); ++i) {
+                if (next % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime ) return next;
+            next++;
+        }
+    }
